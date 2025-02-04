ed07e062a2b354c5f4a78481ac11151e19d9dddc
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-04 20:00:38 +0500

    image functionality

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 const container = document.querySelector(".container");
-const image = document.createElement("img");
+const imageDiv = document.querySelector(".image");
+const timeDiv = document.querySelector(".time");
 
 setInterval(function timeClock(){
     const date = new Date();
-    container.innerHTML = date.toLocaleTimeString();
+    container.querySelector(".time").textContent = date.toLocaleTimeString();
 }, 1000);
 
 function backgroundColor() {
     const hour = new Date().getHours();
     
+    imageDiv.innerHTML = "";
+
+    const image = document.createElement("img");
+
     if(0 <= hour && hour <= 6){
         document.body.style.backgroundColor = "black";
-        
+        image.src = "/images/moon.png"; 
     } 
     else if(6 < hour && hour <= 18){
-        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = "yellow";
+        image.src = "/images/sun.png";
     } 
     else{
         document.body.style.backgroundColor = "black";
+        image.src = "/images/moon.png";
     }
     
+    imageDiv.appendChild(image);
 }
 backgroundColor();
-
-
-
-
-
-
-
-
-
