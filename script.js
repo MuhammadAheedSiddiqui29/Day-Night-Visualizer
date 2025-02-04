55d8a7801db98eab552674f3e5ef5abd74bd3621
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-04 17:17:35 +0500

    background color functionality

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ setInterval(function timeClock(){
     container.innerHTML = date.toLocaleTimeString();
 }, 1000);
 
+function backgroundColor() {
+    const hour = new Date().getHours();
+    
+    if(0 <= hour && hour <= 6){
+        document.body.style.backgroundColor = "black";
+        
+    } 
+    else if(6 < hour && hour <= 18){
+        document.body.style.backgroundColor = "green";
+    } 
+    else{
+        document.body.style.backgroundColor = "black";
+    }
+    
+}
+backgroundColor();
+
 
 
 
